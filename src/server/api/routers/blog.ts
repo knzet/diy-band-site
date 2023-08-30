fb6c48a6669e2b90f56c0b7fdc41282e3660203b
knzet <kat6106@rit.edu>
2023-08-30 15:33:54 -0400

    remove create route

--- src/server/api/routers/blog.ts
+++ src/server/api/routers/blog.ts
@@ -9,16 +9,6 @@ export const blogRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.blogPost.findMany();
   }),
-  newPost: publicProcedure.query(async ({ ctx }) => {
-    const post = await ctx.prisma.blogPost.create({
-      data: {
-        title: "title",
-        content: "content",
-        date: new Date(),
-      },
-    });
-    return post;
-  }),
   // get one needs an input of post id
   getOne: publicProcedure
     .input(z.object({ id: z.string() }))
