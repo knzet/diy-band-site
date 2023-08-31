d35714c89a01c3a7440bb8378e9512312bbdec4b
knzet <kat6106@rit.edu>
2023-08-31 10:35:33 -0400

    header color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,7 +46,7 @@ export default function Home() {
       <main className="gradient-top flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Safety <span className="text-[hsl(280,100%,70%)]">Break</span>
+            Safety <span className="text-[hsl(94,100%,87%)]">Break</span>
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <section
